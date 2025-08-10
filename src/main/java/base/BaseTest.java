06d721070dff425de38169e13d90d37b03526711
Rehan Husain <rehanmansoori6@gmail.com>
2025-08-10 13:53:32 +0530

    second commit

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -5,19 +5,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import utils.Log;
+
+
 public class BaseTest {
 	
 	protected WebDriver driver;
 	
 	@BeforeMethod
 	public void setup() {
+		Log.info("opening chrome");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		Log.info("navigating to url");
 		driver.get("https://admin-demo.nopcommerce.com/");
 	}
 	
 	@AfterMethod
 	public void teardown() {
+		Log.info("closing chrome");
 		if(driver!=null) 
 			driver.quit();
 		}
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,18 +1,25 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.Log;
+
+
 
 public class LoginTest extends BaseTest{
 	
 	@Test
 	public void testLogin() {
 	LoginPage loginPage = new LoginPage(driver);
+	Log.info("entering username and password");
 	loginPage.enterUsername("admin@yourstore.com");
 	loginPage.enterPassword("admin");
-	loginPage.loginButton();	
-
+	loginPage.loginButton();
+	System.out.println("title of page"+ driver.getTitle());
+	Assert.assertEquals(driver.getTitle(), "nopCommerce demo store. Login");
+	Log.info("Assert is successful");
 }
 }
